f40b7ec5c9d03991f69c44858b1d71b2a1c96a01
BT_1N3_16 <BT_1N3_16@BT_1N3_16.bktech.local>
2018-01-08 10:59:24 -0500

    Bootun

---
+++ OrcMathGUI/src/myStuff/SunnyButton.java
@@ -0,0 +1,36 @@
+package myStuff;
+
+import java.awt.Color;
+
+import guiTeacher.Utilities;
+import guiTeacher.components.Action;
+import guiTeacher.components.Button;
+
+public class SunnyButton extends Button implements ButtonInterfaceAndrew{
+
+	public SunnyButton(int x, int y, int w, int h, String text, Action action) {
+		super(x, y, w, h, text, action);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void highlight() {
+		// TODO Auto-generated method stub
+		Color currentColor = getBackground();
+		Utilities.lighten(currentColor, 50);
+	}
+
+	@Override
+	public void setColor(Color color) {
+		// TODO Auto-generated method stub
+		setBackground(color);
+	}
+
+	@Override
+	public void dim() {
+		// TODO Auto-generated method stub
+		Color currentColor = getBackground();
+		Utilities.lighten(currentColor, -50);
+	}
+
+}
